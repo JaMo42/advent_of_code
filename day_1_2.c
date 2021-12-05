e68d207af4c1efb12bf1159c4b1e94cf4d975856
Jakob <jakob@mohrbacher.eu>
2021-12-05 01:05:38 +0100

    Day 1: fix unused values for part two

--- day_1_2.c
+++ day_1_2.c
@@ -19,7 +19,7 @@ int
 main ()
 {
   long *input = NULL;
-  long a, b, c, d;
+  long a, d;
   size_t i;
   unsigned inc = 0;
 
@@ -28,11 +28,11 @@ main ()
   for (i = 0; i < vector_size (input) - 3; ++i)
     {
       a = input[i];
-      b = input[i+1];
-      c = input[i+2];
+      // These are in both sides of the comparison and can be factored out
+      // b = input[i+1];
+      // c = input[i+2];
       d = input[i+3];
 
-      //if (b + c + d > a + b + c)
       if (d > a)
         ++inc;
     }
