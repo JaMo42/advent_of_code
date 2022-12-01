b2c389163dc74506ae086109a0f84bfe1c30b1f8
Jakob <jakob@mohrbacher.eu>
2022-12-01 20:12:09 +0100

    2022 Day 1: Add part 2

--- 2022/day_01.py
+++ 2022/day_01.py
@@ -2,18 +2,16 @@
 from sys import stdin
 
 def main ():
-  print (
-    max (
-      map (
-        lambda elf: sum (
-          [int (cal) for cal in elf.split ('\n')]
-        ),
-        stdin.read ()
-          .strip ()
-          .split ("\n\n")
-      )
-    )
+  elves = map (
+    lambda elf: sum (
+      [int (cal) for cal in elf.split ('\n')]
+    ),
+    stdin.read ()
+      .strip ()
+      .split ("\n\n")
   )
+  print (max (elves))
+  print (sum (sorted (list (elves))[-3:]))
 
 if __name__ == "__main__":
   main ()
